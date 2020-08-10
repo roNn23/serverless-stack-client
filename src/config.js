0489a9ea3bfd76c9914ea93e70ce6f17b131ab7a
Ronny Neefe <ronny.neefe@aoe.com>
2020-08-10 16:28:50 +0200

    added netlify deployment

--- src/config.js
+++ src/config.js
@@ -1,16 +1,42 @@
-export default {
+const dev = {
   s3: {
     REGION: "eu-central-1",
-    BUCKET: "notes-app-uploads-ronn23-ffm",
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-14uuskjg9g6a3",
   },
   apiGateway: {
     REGION: "eu-central-1",
-    URL: "https://1vkxf1cvt9.execute-api.eu-central-1.amazonaws.com/prod",
+    URL: "https://birnl60rxg.execute-api.eu-central-1.amazonaws.com/dev",
   },
   cognito: {
     REGION: "eu-central-1",
-    USER_POOL_ID: "eu-central-1_vx83RYqOs",
-    APP_CLIENT_ID: "77gpe47rciolqdqad8ueubujt0",
-    IDENTITY_POOL_ID: "eu-central-1:56d22428-cd82-45f0-b86c-a003c92837a6",
+    USER_POOL_ID: "eu-central-1_sTLtzVPO0",
+    APP_CLIENT_ID: "2pdrd91dcs5fg2h9590lfredfp",
+    IDENTITY_POOL_ID: "eu-central-1:6b89aeff-f65f-400b-b64d-a2f9e507a4a7",
   },
+};
+
+const prod = {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-hhpkz4jfjqpd",
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://kmnlv5ogd8.execute-api.eu-central-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_Ev9M6TPij",
+    APP_CLIENT_ID: "7mp1tc4oittg747rp5if5d1bff",
+    IDENTITY_POOL_ID: "eu-central-1:a627dc9f-1db6-481a-b5e6-8be328e1b2f5",
+  },
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
 };
